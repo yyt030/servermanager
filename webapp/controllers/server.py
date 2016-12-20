f23878eedf051f31d485f1c8be278d31ea2d253d
yueyt <yyt030@gmail.com>
2016-12-20 13:49:35 +0800

    add server html

--- webapp/controllers/server.py
+++ webapp/controllers/server.py
@@ -2,19 +2,27 @@
 # coding: utf-8
 
 __author__ = 'yueyt'
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 
+from webapp import db
 from webapp.forms.server import ServerForm
+from webapp.models.server import Server
 
 bp = Blueprint('s', __name__)
 
 
-@bp.route('/create')
-def create():
-    name = None
+@bp.route('/add', methods=['GET', 'POST'])
+def add():
     form = ServerForm()
     if form.validate_on_submit():
-        name = form.ip.data
-        pass
+        res = Server.query.filter_by(ip=form.ip.data).first()
+        if res:
+            flash('ip:{}已存在,请确认ip输入是否正确'.format(form.ip.data))
+            return redirect(url_for('.add'))
 
-    return render_template('create.html', active_page='add', form=form, name=name)
+        server = Server(ip=form.ip.data, project=form.project.data, oslevel=form.oslevel.data,
+                        use=form.use.data, status=form.status.data)
+        db.session.add(server)
+        return redirect(url_for('site.index'))
+
+    return render_template('server_add.html', active_page='add', form=form)
--- webapp/forms/server.py
+++ webapp/forms/server.py
@@ -4,10 +4,26 @@
 __author__ = 'yueyt'
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import ip_address
 
 
 class ServerForm(FlaskForm):
-    ip = StringField('ip', validators=[ip_address()])
+    _machine_type_list = [
+        ('SUSE SLES11 SP2', 'SUSE SLES11 SP2'),
+        ('SUSE SLES11 SP3', 'SUSE SLES11 SP3'),
+        ('SUSE SLES11 SP4', 'SUSE SLES11 SP4'),
+        ('AIX 7100', 'AIX 7100'),
+        ('AIX 5100', 'AIX 5100'),
+        ('AIX 6100', 'AIX 6100')
+    ]
+    _project_list = [
+        ('BGSP', 'BGSP'), ('EGSP', 'EGSP')
+    ]
+    ip = StringField('ip:', validators=[ip_address()])
+    project = SelectField('所属项目:', choices=_project_list, coerce=str)
+    oslevel = SelectField('操作系统版本:', choices=_machine_type_list, coerce=str, default='SUSE SLES11 SP3')
+    use = TextAreaField('用途:')
+    status = BooleanField('使用中', default=True)
+
     submit = SubmitField('保存')
