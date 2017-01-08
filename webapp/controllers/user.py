3d0505653ba10b5088fa8dc8b9a41c38ec71fa95
yueyt <yyt030@gmail.com>
2017-01-08 13:38:05 +0800

    update

--- webapp/controllers/user.py
+++ webapp/controllers/user.py
@@ -3,7 +3,7 @@
 
 __author__ = 'yueyt'
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from flask_login import current_user
 
 from webapp import db
@@ -21,11 +21,12 @@ def profile():
 
         db.session.add(current_user)
         db.session.commit()
+        flash('信息已经更新')
     profileform.email.data = current_user.email
     profileform.username.data = current_user.username
     profileform.role_id.data = current_user.role_id
 
-    return render_template('profile.html', profileform=profileform)
+    return render_template('layout_profile.html', profileform=profileform, users=[current_user])
 
 
 @bp.route('/edit')
